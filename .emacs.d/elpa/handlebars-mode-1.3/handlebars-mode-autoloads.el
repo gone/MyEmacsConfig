531b8f696d871b2f1404d84a8d3632fa5d05f729
Ben Beecher <benbeecher@gmail.com>
2012-09-26 14:24:54 -0400

    merge

---
+++ .emacs.d/elpa/handlebars-mode-1.3/handlebars-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; handlebars-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (handlebars-mode) "handlebars-mode" "handlebars-mode.el"
+;;;;;;  (20541 12611))
+;;; Generated autoloads from handlebars-mode.el
+
+(autoload 'handlebars-mode "handlebars-mode" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("handlebars-mode-pkg.el") (20541 12611
+;;;;;;  623853))
+
+;;;***
+
+(provide 'handlebars-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; handlebars-mode-autoloads.el ends here
